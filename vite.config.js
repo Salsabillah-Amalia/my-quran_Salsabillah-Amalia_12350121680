6e23557d64e2ff158135f57ac4743a5e5e7851c6
Salsabillah-Amalia <12350121680@students.uin-suska.ac.id>
2025-04-05 16:21:57 +0700

    siap publish

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/my-quran_Salsabillah-Amalia_1235/',
+  base: '/my-quran_Salsabillah-Amalia_12350121680/',
   plugins: [react()],
 })
